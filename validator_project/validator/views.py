438c076409e3e7d14ace35684117696f6c7bb3e2
mgmdi <14-10444@usb.ve>
2020-04-01 01:28:55 -0400

    added validations to request load

--- validator_project/validator/views.py
+++ validator_project/validator/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from validator.forms import ValidatorForm
 
-# Create your views here.
+class IPNValidator(APIView):
+    """
+    View validate IPN.
+    """
+
+    def post(self, request, format=None):
+        """
+        Returns if IPN is valid.
+        """
+        comment = ValidatorForm(request.data or None)
+        print(comment)
+        res = 'res'
+        return Response(res)
--- validator_project/validator_project/urls.py
+++ validator_project/validator_project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from validator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('payments/paypal/', views.IPNValidator.as_view()),
 ]
