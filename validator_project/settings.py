da00032dde91853c0f4e5dbcf2bfdc14eddd8d54
mgmdi <14-10444@usb.ve>
2020-04-01 14:02:39 -0400

    fixed settings

--- validator_project/settings.py
+++ validator_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'validator_project.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'validator_project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
